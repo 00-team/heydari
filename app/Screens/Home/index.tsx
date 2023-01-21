658c20bfc383708f0d1c29360619452d5439635e
sadra <sadrataghavi1383@gmail.com>
2023-01-21 14:40:47 +0330

    feat(home): welcomer add to home

--- app/Screens/Home/index.tsx
+++ app/Screens/Home/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import HeroSection from './HeroSection'
+import Welcomer from './Welcome'
 
-import "./style/home.scss"
+import './style/home.scss'
 
 const Home = () => {
     return (
         <main className='home'>
             <HeroSection />
+            <Welcomer />
         </main>
     )
 }
