1f06c06b094f2eb361f70a731d8a0085730a497d
sadra <sadrataghavi1383@gmail.com>
2023-01-21 12:04:24 +0330

    feat(hero): hero img

--- app/Screens/Home/HeroSection.tsx
+++ app/Screens/Home/HeroSection.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import './style/hero.scss'
+
+const heroImg = require('../../static/chair.png')
+
 const HeroSection = () => {
-    return <section className='hero-container'>HeroSection</section>
+    return (
+        <section className='hero-container'>
+            <img className='hero-img' src={heroImg} alt='' />
+            <div className='hero-content'></div>
+        </section>
+    )
 }
 
 export default HeroSection
