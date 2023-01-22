54da354817bbefede5c4fef3630ea5418f2a7c49
sadra <sadrataghavi1383@gmail.com>
2023-01-22 18:04:15 +0330

    feat(honors): confetti add and styled

--- app/Screens/Home/Honors.tsx
+++ app/Screens/Home/Honors.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+import { C } from '@00-team/utils'
+
+import Confetti from 'react-confetti'
 
 import './style/honors.scss'
 
 const Honors = () => {
-    return <section className='honors-container'>Honors</section>
+    const LazyRef = useRef<HTMLDivElement>(null)
+    const [isIntersecting, setisIntersecting] = useState(false)
+    const [ConfettiEnd, setConfettiEnd] = useState(false)
+
+    useEffect(() => {
+        if (LazyRef.current && !isIntersecting) {
+            var observer = new IntersectionObserver(
+                ([entry]) => {
+                    if (entry && entry.isIntersecting) {
+                        setisIntersecting(true)
+                        observer.disconnect()
+                    }
+                },
+                {
+                    threshold: 0.4,
+                }
+            )
+
+            observer.observe(LazyRef.current)
+        }
+        return () => {
+            if (observer) observer.disconnect()
+        }
+    }, [LazyRef])
+
+    useEffect(() => {
+        if (!isIntersecting) return
+
+        const timeout = setTimeout(() => {
+            setConfettiEnd(() => true)
+        }, 3000)
+
+        return () => clearTimeout(timeout)
+    }, [isIntersecting])
+
+    console.log(ConfettiEnd)
+
+    return (
+        <section
+            ref={LazyRef}
+            className={`honors-container ${C(isIntersecting)}`}
+        >
+            <h2 className={`section_title honor-header ${C(isIntersecting)}`}>
+                <span>افتخارات ما</span>
+            </h2>
+            <div className='honors-wrapper'></div>
+            {isIntersecting && <Confetti recycle={!ConfettiEnd} />}
+        </section>
+    )
 }
 
 export default Honors
