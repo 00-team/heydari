30e2d01002e07bc6ed3b60f8ce04d735ec061f85
sadra <sadrataghavi1383@gmail.com>
2023-01-22 12:22:50 +0330

    feat(hero)

--- app/Screens/Home/HeroSection.tsx
+++ app/Screens/Home/HeroSection.tsx
@@ -11,39 +11,39 @@ const heroImg = require('../../static/chair.png')
 const HeroSection = () => {
     return (
         <section className='hero-container'>
-            <div className='hero-content'>
-                <div className='title_small typer-wrapper'>
-                    تولید انواع صندلی های{' '}
-                    <Typer MidDelay={2000} words={['اداری', 'لورم', 'منزل']} />{' '}
-                    خود را به ما بسپارید.
-                </div>
-                <span className='hero-anim'>
-                    <div className='section_title company'>
-                        صنایع تولیدی حیدری
+            <div className='hero-wrapper'>
+                <div className='hero-content'>
+                    <div className='title_small typer-wrapper'>
+                        تولید انواع صندلی های{' '}
+                        <span className='title'>
+                            <Typer
+                                MidDelay={2000}
+                                words={['اداری', 'لورم', 'منزل']}
+                            />{' '}
+                        </span>
+                        خود را به ما بسپارید.
                     </div>
-                </span>
-                <span className='hero-anim'>
+                    <div className='title_hero company'>
+                        صنایع تولیدی{' '}
+                        <span className='section_title'>حیدری</span>
+                    </div>
                     <div className='title_smaller detail'>
                         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت
                         چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون
                         هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت
                     </div>
-                </span>
 
-                <div className='cta-wrapper'>
-                    <span className='hero-anim'>
+                    <div className='cta-wrapper'>
                         <StickyButton className='main hero-button'>
                             محصولات
                         </StickyButton>
-                    </span>
-                    <span className='hero-anim'>
                         <button className='sec hero-button'>
                             ارتباط با ما{' '}
                         </button>
-                    </span>
+                    </div>
                 </div>
+                <img className='hero-img' src={heroImg} alt='' />
             </div>
-            <img className='hero-img' src={heroImg} alt='' />
         </section>
     )
 }
