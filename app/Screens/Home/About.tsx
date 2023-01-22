577a2ee9ba4d38aa10560f12bd9adf482bfa7ac6
sadra <sadrataghavi1383@gmail.com>
2023-01-22 12:20:27 +0330

    feat(about): init

---
+++ app/Screens/Home/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './style/about.scss'
+
+const About = () => {
+    return (
+        <section id='about' className='about-container'>
+            About
+        </section>
+    )
+}
+
+export default About
