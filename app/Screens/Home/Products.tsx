cdae152e468e9df101ce468d441b2da52e5464fb
sadra <sadrataghavi1383@gmail.com>
2023-01-25 22:26:56 +0330

    feat(products): section init

---
+++ app/Screens/Home/Products.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+
+import './style/products.scss'
+
+const Products: FC = () => {
+    return (
+        <section className='products-container'>
+            <h2 className='section_title products-header'></h2>
+        </section>
+    )
+}
+
+export default Products
