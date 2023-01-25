9e8ea227b3245206f4202e9c3333c788b20e07d3
sadra <sadrataghavi1383@gmail.com>
2023-01-25 23:17:40 +0330

    fix(prodcuts)

--- app/Screens/Home/Products.tsx
+++ app/Screens/Home/Products.tsx
@@ -1,11 +1,43 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
+
+import { C } from '@00-team/utils'
 
 import './style/products.scss'
 
 const Products: FC = () => {
+    const LazyRef = useRef<HTMLDivElement>(null)
+    const [isIntersecting, setisIntersecting] = useState(false)
+
+    useEffect(() => {
+        if (LazyRef.current && !isIntersecting) {
+            var observer = new IntersectionObserver(
+                ([entry]) => {
+                    if (entry && entry.isIntersecting) {
+                        setisIntersecting(true)
+                        observer.disconnect()
+                    }
+                },
+                {
+                    threshold: 0.4,
+                }
+            )
+
+            observer.observe(LazyRef.current)
+        }
+        return () => {
+            if (observer) observer.disconnect()
+        }
+    }, [LazyRef])
     return (
-        <section className='products-container'>
-            <h2 className='section_title products-header'></h2>
+        <section ref={LazyRef} className='products-container'>
+            <h2
+                className={`section_title products-header ${C(isIntersecting)}`}
+            >
+                <span className='header-wrapper'>
+                    <span>محصولات</span>
+                </span>
+            </h2>
+            <div className="products-wrapper"></div>
         </section>
     )
 }
