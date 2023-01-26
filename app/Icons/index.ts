ce33cb2db1f80d11945467a6bfd8417db072bacb
sadra <sadrataghavi1383@gmail.com>
2023-01-26 17:55:08 +0330

    feat(app)

---
+++ app/Icons/index.ts
@@ -0,0 +1,8 @@
+export * from './products/Color'
+export * from './products/Weight'
+export * from './products/Ruler'
+
+export interface SvgProps {
+    size: number
+    style?: React.CSSProperties
+}
