a196261ede8364ebcf47cb28a312744e681c1221
sadra <sadrataghavi1383@gmail.com>
2023-01-27 17:57:22 +0330

    feat(products)

--- app/Screens/Home/Products.tsx
+++ app/Screens/Home/Products.tsx
@@ -2,67 +2,161 @@ import React, { FC, useEffect, useRef, useState } from 'react'
 
 import { C } from '@00-team/utils'
 
-import { ColorSvg, RulerSvg, WeightSvg } from 'Icons'
+import { ColorSvg, RulerSvg, SvgProps, WeightSvg } from 'Icons'
 
 import './style/products.scss'
 
-const DEBUG_PRODCUTS = [
+interface ProductsModelProps {
+    img: string
+    name: string
+    details: {
+        holder: string
+        Svg: React.FC<SvgProps>
+        data: string
+    }[]
+    link: string
+}
+
+const DEBUG_PRODCUTS: ProductsModelProps[] = [
     {
         img: 'https://picsum.photos/1021/960',
-        details: {
-            name: 'صندلی اداری مدل 123',
-            color: 'آبی',
-            weight: '3',
-            height: '180',
-            width: '80',
-            link: '/',
-        },
+        name: 'صندلی اداری مدل 4312',
+        details: [
+            {
+                holder: 'رنگ',
+                Svg: ColorSvg,
+                data: 'ابی',
+            },
+            {
+                holder: 'طول',
+                Svg: RulerSvg,
+                data: '123',
+            },
+            {
+                holder: 'عرض',
+                Svg: RulerSvg,
+                data: '33',
+            },
+            {
+                holder: 'وزن',
+                Svg: WeightSvg,
+                data: '3',
+            },
+        ],
+        link: '/',
     },
     {
-        img: 'https://picsum.photos/1023/960',
-        details: {
-            name: 'صندلی اداری مدل 321',
-            color: 'آبی',
-            weight: '3',
-            height: '180',
-            width: '80',
-            link: '/',
-        },
+        img: 'https://picsum.photos/1022/959',
+        name: 'صندلی اداری مدل 213',
+        details: [
+            {
+                holder: 'رنگ',
+                Svg: ColorSvg,
+                data: 'بنفش',
+            },
+            {
+                holder: 'طول',
+                Svg: RulerSvg,
+                data: '132',
+            },
+            {
+                holder: 'عرض',
+                Svg: RulerSvg,
+                data: '31',
+            },
+            {
+                holder: 'وزن',
+                Svg: WeightSvg,
+                data: '1',
+            },
+        ],
+        link: '/',
     },
     {
-        img: 'https://picsum.photos/1021/961',
-        details: {
-            name: 'صندلی اداری مدل 2312',
-            color: 'آبی',
-            weight: '3',
-            height: '180',
-            width: '80',
-            link: '/',
-        },
-    },
-    {
-        img: 'https://picsum.photos/1022/962',
-        details: {
-            name: 'صندلی اداری مدل 2312',
-            color: 'آبی',
-            weight: '3',
-            height: '180',
-            width: '80',
-            link: '/',
-        },
+        img: 'https://picsum.photos/1022/961',
+        name: 'صندلی اداری مدل 321',
+        details: [
+            {
+                holder: 'رنگ',
+                Svg: ColorSvg,
+                data: 'سیاه',
+            },
+            {
+                holder: 'طول',
+                Svg: RulerSvg,
+                data: '142',
+            },
+            {
+                holder: 'عرض',
+                Svg: RulerSvg,
+                data: '41',
+            },
+            {
+                holder: 'وزن',
+                Svg: WeightSvg,
+                data: '5',
+            },
+        ],
+        link: '/',
     },
     {
         img: 'https://picsum.photos/1021/963',
-        details: {
-            name: 'صندلی اداری مدل 2312',
-            color: 'آبی',
-            weight: '3',
-            height: '180',
-            width: '80',
-            link: '/',
-        },
+        name: 'صندلی اداری مدل 124',
+        details: [
+            {
+                holder: 'رنگ',
+                Svg: ColorSvg,
+                data: 'زرد',
+            },
+            {
+                holder: 'طول',
+                Svg: RulerSvg,
+                data: '150',
+            },
+            {
+                holder: 'عرض',
+                Svg: RulerSvg,
+                data: '20',
+            },
+            {
+                holder: 'وزن',
+                Svg: WeightSvg,
+                data: '4',
+            },
+        ],
+        link: '/',
+    },
+    {
+        img: 'https://picsum.photos/1024/963',
+        name: 'صندلی اداری مدل 213',
+        details: [
+            {
+                holder: 'رنگ',
+                Svg: ColorSvg,
+                data: 'نارنجی',
+            },
+            {
+                holder: 'طول',
+                Svg: RulerSvg,
+                data: '100',
+            },
+            {
+                holder: 'عرض',
+                Svg: RulerSvg,
+                data: '30',
+            },
+            {
+                holder: 'وزن',
+                Svg: WeightSvg,
+                data: '30',
+            },
+        ],
+        link: '/',
     },
 ]
+
+// BASE_PRODUCT_DETAIL_DELAY
+const BPDD = 0.25
 
 const Products: FC = () => {
     const LazyRef = useRef<HTMLDivElement>(null)
@@ -105,6 +199,15 @@ const Products: FC = () => {
 const ProductsWrapper: FC = () => {
     const [ActiveProduct, setActiveProduct] = useState(1)
 
+    const returnClass = (idx0: number): string => {
+        if (ActiveProduct === idx0) return 'active'
+        if (ActiveProduct - 1 === idx0) return 'next'
+        if (ActiveProduct + 1 === idx0) return 'prev'
+        if (ActiveProduct === DEBUG_PRODCUTS.length - 1 && idx0 === 0)
+            return 'prev'
+        return ''
+    }
+
     useEffect(() => {
         const inverval = setInterval(() => {
             setActiveProduct(value => {
@@ -120,71 +223,66 @@ const ProductsWrapper: FC = () => {
 
     return (
         <div className='products-wrapper'>
-            <div className='product-detail-wrapper'>
-                <div className='product-name section_title'>لورم ایپسوم</div>
-                <div className='product-details title'>
-                    <div className='product-detail '>
-                        <div className='holder'>
-                            <div className='holder-icon icon'>
-                                <ColorSvg size={25} />
+            <div className='product-detail-container'>
+                <div className='product-name-wrapper'>
+                    {DEBUG_PRODCUTS.map(({ name }, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className={`product-name section_title ${returnClass(
+                                    index
+                                )}`}
+                            >
+                                {name}
                             </div>
-                            <div className='holder-text'>رنگ:</div>
-                        </div>
-                        <div className='data'>نارنجی</div>
-                    </div>
-                    <div className='product-detail'>
-                        <div className='holder'>
-                            <div className='holder-icon icon'>
-                                <WeightSvg size={25} />
-                            </div>
-                            <div className='holder-text'>وزن:</div>
-                        </div>
-                        <div className='data'>3</div>
-                    </div>
-                    <div className='product-detail'>
-                        <div className='holder'>
-                            <div className='holder-icon icon'>
-                                <RulerSvg size={25} />
-                            </div>
-                            <div className='holder-text'>طول:</div>
-                        </div>
-                        <div className='data'>60</div>
-                    </div>
-                    <div className='product-detail'>
-                        <div className='holder'>
-                            <div className='holder-icon icon'>
-                                <RulerSvg
-                                    style={{ rotate: `-90deg` }}
-                                    size={25}
-                                />
-                            </div>
-                            <div className='holder-text'>عرض:</div>
-                        </div>
-                        <div className='data'>60</div>
-                    </div>
+                        )
+                    })}
                 </div>
+                <div className='product-details-wrapper'>
+                    {DEBUG_PRODCUTS.map(({ details }, idx1) => {
+                        console.log(idx1)
+                        return (
+                            <div key={idx1} className='product-details title'>
+                                {details.map(({ holder, data, Svg }, idx2) => {
+                                    return (
+                                        <div
+                                            className={`product-detail ${returnClass(
+                                                idx1
+                                            )}`}
+                                            style={{
+                                                transitionDelay: `${
+                                                    BPDD * idx2
+                                                }s`,
+                                            }}
+                                            key={idx2}
+                                        >
+                                            <div className='holder'>
+                                                <div className='holder-icon icon'>
+                                                    <Svg size={25} />
+                                                </div>
+                                                <div className='holder-text'>
+                                                    {holder}:
+                                                </div>
+                                            </div>
+                                            <div className='data'>{data}</div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+                </div>
+
                 <button className='product-submit title_small'>
                     خرید فوری!
                 </button>
             </div>
             <div className='products-slider'>
                 {DEBUG_PRODCUTS.map(({ img }, idx0) => {
-                    const returnClass = (): string => {
-                        if (ActiveProduct === idx0) return 'active'
-                        if (ActiveProduct - 1 === idx0) return 'next'
-                        if (ActiveProduct + 1 === idx0) return 'prev'
-                        if (
-                            ActiveProduct === DEBUG_PRODCUTS.length - 1 &&
-                            idx0 === 0
-                        )
-                            return 'prev'
-                        return ''
-                    }
-
                     return (
                         <div
                             key={idx0}
-                            className={`product-slider ${returnClass()}`}
+                            className={`product-slider ${returnClass(idx0)}`}
                         >
                             <img
                                 loading='lazy'
