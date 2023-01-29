2cd49d317645b296020354d6a315be0640fd1295
sadra <sadrataghavi1383@gmail.com>
2023-01-29 18:37:21 +0330

    fix(footer): footer header

--- app/layout/Footer.tsx
+++ app/layout/Footer.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { FC } from 'react'
+
+import { CallSvg, LocationSvg, SvgProps } from 'Icons'
 
 import './style/footer.scss'
 
 const Footer = () => {
-    return <footer className='footer-container'>Footer</footer>
+    return (
+        <footer className='footer-container'>
+            <div className='footer-header'>
+                <FooterHeaderRow
+                    Svg={LocationSvg}
+                    data='یوسف اباد، پلاک 21، زنگ 5'
+                    holder='آدرس'
+                />
+                <FooterHeaderRow
+                    Svg={CallSvg}
+                    data='09120974957'
+                    holder='شماره تماس'
+                />
+                <FooterHeaderRow
+                    Svg={CallSvg}
+                    data='09120974957'
+                    holder='شماره تماس'
+                />
+            </div>
+        </footer>
+    )
+}
+
+interface FooterHeaderRow {
+    Svg: React.FC<SvgProps>
+    holder: string
+    data: string
+}
+
+const FooterHeaderRow: FC<FooterHeaderRow> = ({ Svg, data, holder }) => {
+    return (
+        <div className='footer-header-row title_small'>
+            <div className='header-icon icon'>
+                <div className='icon-wrapper'>
+                    <Svg size={35} />
+                </div>
+            </div>
+            <div className='header-wrapper '>
+                <div className='holder title_smaller'>{holder} </div>
+                <div className='data '>{data}</div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
