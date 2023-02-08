835c31330743a4bc63c33f346583bcf275c2de94
sadra <sadrataghavi1383@gmail.com>
2023-02-08 14:24:30 +0330

    refactor(icons)

--- app/Icons/footer/Location.tsx
+++ app/Icons/footer/Location.tsx
@@ -7,7 +7,7 @@ export const LocationSvg: FC<SvgProps> = ({ size, style }) => {
         <svg
             stroke='currentColor'
             fill='currentColor'
-            stroke-width='0'
+            strokeWidth={0}
             viewBox='0 0 24 24'
             height={size}
             width={size}
