1691641b780b30ed8774ab446dddf55c4c52293d
sadra <sadrataghavi1383@gmail.com>
2023-02-08 14:23:57 +0330

    refactor(icons)

--- app/Icons/footer/Call.tsx
+++ app/Icons/footer/Call.tsx
@@ -7,7 +7,7 @@ export const CallSvg: FC<SvgProps> = ({ size, style }) => {
         <svg
             stroke='currentColor'
             fill='currentColor'
-            stroke-width='0'
+            strokeWidth={0}
             viewBox='0 0 512 512'
             height={size}
             width={size}
