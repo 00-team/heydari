39de1199a15e137c533fed024a6efe7742a45d6e
sadra <sadrataghavi1383@gmail.com>
2023-02-18 21:15:00 +0330

    fix(icons)

--- app/Icons/models/Arrow.tsx
+++ app/Icons/models/Arrow.tsx
@@ -13,9 +13,9 @@ export const ArrowSvg: Icon = ({ size, ...attr }) => {
             {...attr}
         >
             <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
                 d='M17 8l4 4m0 0l-4 4m4-4H3'
             ></path>
         </svg>
