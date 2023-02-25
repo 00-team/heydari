fff218e46c9e4b8e9e10511a91aa7b97f2ebe61b
sadra <sadrataghavi1383@gmail.com>
2023-02-25 21:05:44 +0330

    fix(product): page init

---
+++ app/Screens/Product/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import './style/product.scss'
+
+const Product: FC = () => {
+    return <div className='product-container'>Product</div>
+}
+
+export default Product
