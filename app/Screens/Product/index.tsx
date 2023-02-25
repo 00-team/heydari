1bda1524d2c98fdb0e6a2a586d2f5413dc471247
sadra <sadrataghavi1383@gmail.com>
2023-02-25 22:04:45 +0330

    fix(product): product imgs init

--- app/Screens/Product/index.tsx
+++ app/Screens/Product/index.tsx
@@ -3,7 +3,23 @@ import React, { FC } from 'react'
 import './style/product.scss'
 
 const Product: FC = () => {
-    return <div className='product-container'>Product</div>
+    return (
+        <div className='product-container'>
+            <div className='product-wrapper'>
+                <div className='product-detials'></div>
+                <ProductImgs />
+            </div>
+        </div>
+    )
+}
+
+const ProductImgs: FC = () => {
+    return (
+        <div className='product-imgs'>
+            <img className='main-img' src='https://picsum.photos/1080/700' />
+            <div className='other-imgs'></div>
+        </div>
+    )
 }
 
 export default Product
