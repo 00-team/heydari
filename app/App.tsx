25dbef499ca56ef99447557b069d92153e7420df
sadra <sadrataghavi1383@gmail.com>
2023-07-02 15:29:30 +0330

    feat(app): contact init

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect } from 'react'
 
+import ContactUs from 'Screens/ContactUs'
 import Home from 'Screens/Home'
 import Product from 'Screens/Product'
 import { Footer, Navbar } from 'layout'
@@ -31,6 +32,7 @@ const MainContent: FC = () => {
         <Routes>
             <Route index element={<Home />} />
             <Route path='/product' element={<Product />} />
+            <Route path='/contact' element={<ContactUs />} />
         </Routes>
     )
 }
