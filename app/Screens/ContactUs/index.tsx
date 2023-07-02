30e9e8c266b76967f49e56564c6e0b9abafe60d1
sadra <sadrataghavi1383@gmail.com>
2023-07-02 15:29:46 +0330

    feat(contact): init

---
+++ app/Screens/ContactUs/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import './style/contactus.scss'
+
+const ContactUs: FC = () => {
+    return <div className='contact-container'>ContactUs</div>
+}
+
+export default ContactUs
