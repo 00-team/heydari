47091d0fd7b751fd7b6fd6de3cfcd26402f38d9b
sadra <sadrataghavi1383@gmail.com>
2023-07-03 14:31:14 +0330

    feat(contact)

--- app/Screens/ContactUs/index.tsx
+++ app/Screens/ContactUs/index.tsx
@@ -43,6 +43,7 @@ const ContactUs: FC = () => {
                         src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.8468496861897!2d51.39778397484384!3d35.72998486503635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e012d4c92106d%3A0x7b7eec5872b4ff0!2sTehran%20Province%2C%20Tehran%2C%2021St%20St%2C%20Iran!5e0!3m2!1sen!2s!4v1688300364495!5m2!1sen!2s'
                         allowFullScreen
                         loading='lazy'
+                        height={500}
                     ></iframe>
                 </aside>
             </div>
