355bbc3fefe12d2282fb08841186c69818b06518
sadra <sadrataghavi1383@gmail.com>
2023-07-03 20:51:38 +0330

    feat(about): header add

--- app/Screens/About/index.tsx
+++ app/Screens/About/index.tsx
@@ -2,8 +2,25 @@ import React, { FC } from 'react'
 
 import './style/about.scss'
 
+const headerImg = require('../../static/about/header.jpg')
+
 const About: FC = () => {
-    return <div className='about-container'>About</div>
+    return (
+        <div className='about-container'>
+            <header
+                className='about-header'
+                style={{
+                    backgroundImage: `
+    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${headerImg})`,
+                }}
+            >
+                <div className='header-title title_hero'>
+                    درباره <span>حیدری</span>
+                </div>
+            </header>
+        </div>
+    )
 }
 
 export default About
