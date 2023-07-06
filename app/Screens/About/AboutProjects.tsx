bd612d80fbfe1dd5084d90fda6fb6693f999125a
sadra <sadrataghavi1383@gmail.com>
2023-07-06 16:55:29 +0330

    feat(about): projects update

--- app/Screens/About/AboutProjects.tsx
+++ app/Screens/About/AboutProjects.tsx
@@ -34,32 +34,40 @@ export const AboutProjects: FC = () => {
             <h2 className={`project-title title_hero ${C(Isintersected)}`}>
                 <span>پروژه های ما</span>
             </h2>
-            <div className='projects-wrapper'>
-                <div className='project prev'>
-                    <img
-                        src={'https://picsum.photos/1920/1080'}
-                        decoding={'async'}
-                        loading='lazy'
-                        alt=''
-                    />
-                </div>
-                <div className='project active'>
-                    <img
-                        src={'https://picsum.photos/1920/1080'}
-                        decoding={'async'}
-                        loading='lazy'
-                        alt=''
-                    />
-                </div>
-                <div className='project next'>
-                    <img
-                        src={'https://picsum.photos/1920/1080'}
-                        decoding={'async'}
-                        loading='lazy'
-                        alt=''
-                    />
-                </div>
-            </div>
+            <ProjectsSlider Isintersected={Isintersected} />
         </section>
+    )
+}
+
+const DEBUG_SLIDES = [
+    'https://picsum.photos/1920/1079',
+    'https://picsum.photos/1920/1081',
+    'https://picsum.photos/1920/1083',
+    'https://picsum.photos/1920/1078',
+    'https://picsum.photos/1920/1085',
+    'https://picsum.photos/1920/1084',
+]
+
+interface ProjectsSliderProps {
+    Isintersected: boolean
+}
+
+const ProjectsSlider: FC<ProjectsSliderProps> = ({ Isintersected }) => {
+    return (
+        <div className={`projects-wrapper ${C(Isintersected)}`}>
+            {DEBUG_SLIDES.map((img, index) => {
+                return (
+                    <div className='project' key={index}>
+                        <img
+                            src={img}
+                            decoding={'async'}
+                            loading='lazy'
+                            draggable={false}
+                            alt=''
+                        />
+                    </div>
+                )
+            })}
+        </div>
     )
 }
