54546400788ae5d8e4d44fc1ee439493964f82c9
sadra <sadrataghavi1383@gmail.com>
2023-07-06 15:07:16 +0330

    feat(aboutProjects): projects section init

---
+++ app/Screens/About/AboutProjects.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+export const AboutProjects: FC = () => {
+    return (
+        <section className='about-projects'>
+            <h2 className='project-title title_hero'>
+                <span>پروژه های ما</span>
+            </h2>
+            <div className='projects-wrapper'></div>
+        </section>
+    )
+}
