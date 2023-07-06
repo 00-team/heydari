429a18ce453f63c286448b7f052cd83437e6e2e2
sadra <sadrataghavi1383@gmail.com>
2023-07-06 15:13:16 +0330

    feat(about): intersecting user

--- app/Screens/About/AboutProjects.tsx
+++ app/Screens/About/AboutProjects.tsx
@@ -1,9 +1,35 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
+
+import { C } from '@00-team/utils'
 
 export const AboutProjects: FC = () => {
+    const container = useRef<HTMLElement>(null)
+    const [Isintersected, setIsintersected] = useState(false)
+
+    useEffect(() => {
+        if (container.current && !Isintersected) {
+            var observer = new IntersectionObserver(
+                ([entry]) => {
+                    if (entry && entry.isIntersecting) {
+                        setIsintersected && setIsintersected(true)
+                        observer.disconnect()
+                    }
+                },
+                {
+                    threshold: 0.2,
+                }
+            )
+
+            observer.observe(container.current)
+        }
+        return () => {
+            if (observer) observer.disconnect()
+        }
+    }, [container])
+
     return (
-        <section className='about-projects'>
-            <h2 className='project-title title_hero'>
+        <section ref={container} className='about-projects'>
+            <h2 className={`project-title title_hero ${C(Isintersected)}`}>
                 <span>پروژه های ما</span>
             </h2>
             <div className='projects-wrapper'></div>
