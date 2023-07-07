cdffa292677272666344193c438c054a3b746ec6
sadra <sadrataghavi1383@gmail.com>
2023-07-07 23:15:11 +0330

    feat(about): honors

--- app/Screens/About/AboutHonors.tsx
+++ app/Screens/About/AboutHonors.tsx
@@ -1,9 +1,41 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
+
+import { C } from '@00-team/utils'
 
 import './style/aboutHonors.scss'
 
 const AboutHonors: FC = () => {
-    return <section className='about-page-honors'>AboutHonors</section>
+    const container = useRef<HTMLElement>(null)
+    const [Isintersected, setIsintersected] = useState(false)
+
+    useEffect(() => {
+        if (container.current && !Isintersected) {
+            var observer = new IntersectionObserver(
+                ([entry]) => {
+                    if (entry && entry.isIntersecting) {
+                        setIsintersected && setIsintersected(true)
+                        observer.disconnect()
+                    }
+                },
+                {
+                    threshold: 0.2,
+                }
+            )
+
+            observer.observe(container.current)
+        }
+        return () => {
+            if (observer) observer.disconnect()
+        }
+    }, [container])
+
+    return (
+        <section className='about-page-honors' ref={container}>
+            <h2 className={`honors-title title_hero ${C(Isintersected)}`}>
+                <span>افتخارات</span>
+            </h2>
+        </section>
+    )
 }
 
 export default AboutHonors
