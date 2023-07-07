b7ae6377986d825bfb2ff343c3bb4e2e3bfffe97
sadra <sadrataghavi1383@gmail.com>
2023-07-07 22:57:00 +0330

    feat(about): honors section init

---
+++ app/Screens/About/AboutHonors.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import './style/aboutHonors.scss'
+
+const AboutHonors: FC = () => {
+    return <section className='about-page-honors'>AboutHonors</section>
+}
+
+export default AboutHonors
