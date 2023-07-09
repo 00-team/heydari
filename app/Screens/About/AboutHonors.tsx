b44ae817ac84ae0f15189c10d0b208d8bd63dc03
sadra <sadrataghavi1383@gmail.com>
2023-07-09 16:26:18 +0330

    feat(about)

--- app/Screens/About/AboutHonors.tsx
+++ app/Screens/About/AboutHonors.tsx
@@ -4,11 +4,13 @@ import { C } from '@00-team/utils'
 
 import './style/aboutHonors.scss'
 
-const DEBUG_HONOR1 = require('../../static/home/honors/honor1.png')
-const DEBUG_HONOR2 = require('../../static/home/honors/honor2.png')
-const DEBUG_HONOR3 = require('../../static/home/honors/honor3.png')
-const DEBUG_HONOR4 = require('../../static/home/honors/honor4.png')
-const DEBUG_HONOR5 = require('../../static/home/honors/honor5.png')
+const honors = [
+    { img: require('../../static/home/honors/honor1.png') },
+    { img: require('../../static/home/honors/honor2.png') },
+    { img: require('../../static/home/honors/honor3.png') },
+    { img: require('../../static/home/honors/honor4.png') },
+    { img: require('../../static/home/honors/honor5.png') },
+]
 
 const AboutHonors: FC = () => {
     const container = useRef<HTMLElement>(null)
@@ -48,48 +50,37 @@ const AboutHonors: FC = () => {
 interface HonorsWrapperProps {}
 
 const HonorsWrapper: FC<HonorsWrapperProps> = () => {
+    const returnTransform = (index: number) => {
+        var middle = Math.floor((honors.length - 1) / 2)
+
+        if (index === middle) return 0
+        if (index - 1 === middle || index + 1 === middle) return 50
+        if (index - 2 === middle || index + 2 === middle) return 70
+        return 0
+    }
+
     return (
         <div className='honors-wrapper'>
-            <div className='honor-card'>
-                <img
-                    src={DEBUG_HONOR1}
-                    decoding={'async'}
-                    loading={'lazy'}
-                    alt=''
-                />
-            </div>
-            <div className='honor-card'>
-                <img
-                    src={DEBUG_HONOR2}
-                    decoding={'async'}
-                    loading={'lazy'}
-                    alt=''
-                />
-            </div>
-            <div className='honor-card'>
-                <img
-                    src={DEBUG_HONOR3}
-                    decoding={'async'}
-                    loading={'lazy'}
-                    alt=''
-                />
-            </div>
-            <div className='honor-card'>
-                <img
-                    src={DEBUG_HONOR4}
-                    decoding={'async'}
-                    loading={'lazy'}
-                    alt=''
-                />
-            </div>
-            <div className='honor-card'>
-                <img
-                    src={DEBUG_HONOR5}
-                    decoding={'async'}
-                    loading={'lazy'}
-                    alt=''
-                />
-            </div>
+            {honors.map(({ img }, index) => {
+                return (
+                    <div
+                        className='honor-card'
+                        key={index}
+                        style={{
+                            transform: `translateY(${returnTransform(
+                                index
+                            )}px)`,
+                        }}
+                    >
+                        <img
+                            src={img}
+                            decoding={'async'}
+                            loading={'lazy'}
+                            alt=''
+                        />
+                    </div>
+                )
+            })}
         </div>
     )
 }
