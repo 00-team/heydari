451601c4cb88378f75be0e7fb238c9574b03d35d
sadra <sadrataghavi1383@gmail.com>
2023-07-11 13:36:56 +0330

    feat(products): page init

---
+++ app/Screens/Products/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import './style/products.scss'
+
+const Products: FC = () => {
+    return <div className='products-page-container'>Products</div>
+}
+
+export default Products
