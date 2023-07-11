23486beea9bfd2a2b4de9c43977d58fdee072f7a
sadra <sadrataghavi1383@gmail.com>
2023-07-11 15:58:48 +0330

    feat(products)

--- app/Screens/Products/index.tsx
+++ app/Screens/Products/index.tsx
@@ -7,7 +7,13 @@ const Products: FC = () => {
         window.scrollTo(0, 0)
     }, [])
 
-    return <div className='products-page-container'>Products</div>
+    return (
+        <div className='products-page-container'>
+            <header className='products-header'>
+                <div className='header-title'></div>
+            </header>
+        </div>
+    )
 }
 
 export default Products
