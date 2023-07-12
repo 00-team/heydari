ede8c0a2c4b106a1c5496ed215bd3589400c0d25
sadra <sadrataghavi1383@gmail.com>
2023-07-12 13:42:08 +0330

    feat(products)

--- app/Screens/Products/index.tsx
+++ app/Screens/Products/index.tsx
@@ -5,8 +5,8 @@ import './style/products.scss'
 const headerImg = require('../../static/products/header.jpg')
 
 const productImg = require('../../static/products/1.png')
-// const productImg2 = require('../../static/products/2.png')
-// const productImg3 = require('../../static/products/3.png')
+const productImg2 = require('../../static/products/2.png')
+const productImg3 = require('../../static/products/3.png')
 
 const Products: FC = () => {
     useEffect(() => {
@@ -26,28 +26,52 @@ url(${headerImg})`,
                 <div className='header-title title_hero'>محصولات</div>
             </header>
             <section className='products-wrapper'>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+                <ProductCard
+                    title='پاف کد F520 چهار پایه پلاستیکی'
+                    img={productImg}
+                />
+                <ProductCard
+                    title='پاف کد F520 چهار پایه پلاستیکی'
+                    img={productImg2}
+                />
+                <ProductCard
+                    title='پاف کد F520 چهار پایه پلاستیکی'
+                    img={productImg3}
+                />
+                <ProductCard
+                    title='پاف کد F520 چهار پایه پلاستیکی'
+                    img={productImg3}
+                />
+                <ProductCard
+                    title='پاف کد F520 چهار پایه پلاستیکی'
+                    img={productImg2}
+                />
+                <ProductCard
+                    title='پاف کد F520 چهار پایه پلاستیکی'
+                    img={productImg}
+                />
             </section>
         </main>
     )
 }
 
-const ProductCard: FC = () => {
+interface ProductCardProps {
+    img: string
+    title: string
+}
+
+const ProductCard: FC<ProductCardProps> = ({ img, title }) => {
     return (
         <figure className='product-card'>
             <img
                 className='card-img'
-                src={productImg}
+                src={img}
                 decoding={'async'}
                 loading={'lazy'}
                 alt=''
             />
             <figcaption className='card-caption title_smaller'>
-                پاف کد F520 چهار پایه پلاستیکی
+                {title}
             </figcaption>
         </figure>
     )
