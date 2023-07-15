596b2969bca9b2cadc592b4d46795896515b7377
sadra <sadrataghavi1383@gmail.com>
2023-07-15 13:50:56 +0330

    feat(blogs): img header

--- app/Screens/Blogs/index.tsx
+++ app/Screens/Blogs/index.tsx
@@ -2,8 +2,24 @@ import React, { FC } from 'react'
 
 import './style/blogs.scss'
 
+const headerImg = require('../../static/blogs/header.jpg')
+
 const Blogs: FC = () => {
-    return <main className='blogs-container'>Blogs</main>
+    return (
+        <main className='blogs-container'>
+            <header
+                className='products-header'
+                style={{
+                    backgroundImage: `
+linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+url(${headerImg})`,
+                }}
+            >
+                <h2 className='header-title title_hero'>مقالات</h2>
+            </header>
+            <div className='blogs-wrapper'></div>
+        </main>
+    )
 }
 
 export default Blogs
