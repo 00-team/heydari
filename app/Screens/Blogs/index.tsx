01b73d45a7fcc94e659e708b2d9e48016bdc82cb
sadra <sadrataghavi1383@gmail.com>
2023-07-15 14:14:01 +0330

    feat(blogs)

--- app/Screens/Blogs/index.tsx
+++ app/Screens/Blogs/index.tsx
@@ -17,8 +17,35 @@ url(${headerImg})`,
             >
                 <h2 className='header-title title_hero'>مقالات</h2>
             </header>
-            <div className='blogs-wrapper'></div>
+            <section className='blogs-wrapper'>
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+            </section>
         </main>
+    )
+}
+
+const BlogCard: FC = () => {
+    return (
+        <figure className='blog-card'>
+            <img
+                className='card-img'
+                src='https://picsum.photos/1920/1080'
+                alt=''
+            />
+            <figcaption className='card-caption title_smaller'>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                استفاده از طراحان گرافیک است لورم ایپسوم متن ساختگی با تولید
+                سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است لورم
+                ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                استفاده از طراحان گرافیک است
+            </figcaption>
+
+            <button className='btn-container title_smaller  '>دیدن</button>
+        </figure>
     )
 }
 
