83c6b5c7f79ab81a78c77c9f667b7d419d7b1dfa
sadra <sadrataghavi1383@gmail.com>
2023-07-16 13:59:06 +0330

    feat(menu): small nav icon

--- app/layout/Navbar.tsx
+++ app/layout/Navbar.tsx
@@ -3,6 +3,7 @@ import React, { FC, useEffect, useState } from 'react'
 import { C } from '@00-team/utils'
 
 import { BlogsSvg, CallSvg, ChairSvg, CompanySvg, HomeSvg } from 'Icons'
+import { MenuSvg } from 'Icons/navbar/Menu'
 import { Link } from 'react-router-dom'
 
 import './style/navbar.scss'
@@ -78,5 +79,52 @@ const BigNav: FC = () => {
 }
 
 const SmallNav: FC = () => {
-    return <nav className='small-nav-container'></nav>
+    return (
+        <nav className='small-nav-container'>
+            <MenuSvg className='menu-icon' size={40} />
+            <img
+                className='nav-logo'
+                src={navLogo}
+                decoding={'async'}
+                loading={'lazy'}
+                alt=''
+            />
+            <div className='nav-wrapper'>
+                <img
+                    className='nav-logo'
+                    src={navLogo}
+                    decoding={'async'}
+                    loading={'lazy'}
+                    alt=''
+                />
+
+                <section className='columns-wrapper'>
+                    <div className='nav-column title_small'>
+                        <HomeSvg size={30} />
+                        <div className='holder'>خانه</div>
+                    </div>
+                    <div className='nav-column title_small'>
+                        <ChairSvg size={30} />
+                        <div className='holder'>محصولات</div>
+                    </div>
+                    <div className='nav-column title_small'>
+                        <CallSvg size={30} />
+                        <div className='holder'>ارتباط با ما</div>
+                    </div>
+                    <div className='nav-column title_small'>
+                        <CompanySvg size={30} />
+                        <div className='holder'> درباره ما </div>
+                    </div>
+                    <div className='nav-column title_small'>
+                        <BlogsSvg size={30} />
+                        <div className='holder'> مقالات </div>
+                    </div>
+                </section>
+
+                {/* debug */}
+                <div></div>
+                {/* debug */}
+            </div>
+        </nav>
+    )
 }
