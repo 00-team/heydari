cee5afd3160db6d4fbd4cff429bffb2a53792262
sadra <sadrataghavi1383@gmail.com>
2023-07-16 14:28:53 +0330

    refactor(icons)

--- app/Icons/products/Color.tsx
+++ app/Icons/products/Color.tsx
@@ -1,8 +1,6 @@
-import React, { FC } from 'react'
+import React from 'react'
 
-import { SvgProps } from 'Icons'
-
-export const ColorSvg: FC<SvgProps> = ({ size, ...attr }) => {
+export const ColorSvg: Icon = ({ size, ...attr }) => {
     return (
         <svg
             stroke='currentColor'
