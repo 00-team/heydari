e93ef7917c604bb759a11d3e292afe7b79c2187b
sadra <sadrataghavi1383@gmail.com>
2023-07-16 13:58:41 +0330

    feat(menu): small nav icon

---
+++ app/Icons/Navbar/Menu.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+
+import { SvgProps } from 'Icons'
+
+export const MenuSvg: FC<SvgProps> = ({ size, ...attr }) => {
+    return (
+        <svg
+            stroke='currentColor'
+            fill='currentColor'
+            stroke-width='0'
+            viewBox='0 0 20 20'
+            aria-hidden='true'
+            height={size}
+            width={size}
+            xmlns='http://www.w3.org/2000/svg'
+            {...attr}
+        >
+            <path
+                fill-rule='evenodd'
+                d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+                clip-rule='evenodd'
+            ></path>
+        </svg>
+    )
+}
