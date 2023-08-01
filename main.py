e948b2d9321f133924121a3ec7f9be38e973c14c
007 <dr007cc@gmail.com>
2023-08-01 23:37:59 +0330

    fix: update main.py

--- main.py
+++ main.py
@@ -4,53 +4,38 @@ from pathlib import Path
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.routing import APIRoute
+from fastapi.staticfiles import StaticFiles
 
-# from fastapi.staticfiles import StaticFiles
+from deps import get_ip
+from shared import settings
 
-
-BASE_DIR = Path(__file__).parent
-
-index_path = BASE_DIR / 'static/dist/index.html'
+index_path = settings.base_dir / 'static/dist/index.html'
 INDEX_HTML = 'index.html not found :/'
 if index_path.is_file():
     with open(index_path, 'r') as f:
         INDEX_HTML = f.read()
 
 
-# TODO: make the doc
 app = FastAPI(
-    # title='Heydari',
-    # version='0.0.1',
-    # description='**Heydari api documents**',
-
-    # openapi_url=None
+    title='Heydari',
+    version='0.0.1',
+    description='**Heydari api documents**',
+    dependencies=[get_ip()]
 )
 
-# app.mount('/media', StaticFiles(directory='media'), name='media')
-
-# api = APIRouter(
-#     prefix='/api',
-# )
+if settings.debug:
+    app.mount('/static', StaticFiles(directory='static'), name='static')
 
 
 @app.on_event('startup')
 async def startup():
-    # await redis.ping()
-    # await database.connect()
     pass
 
 
 @app.on_event('shutdown')
 async def shutdown():
-    # await redis.connection_pool.disconnect()
-    # await database.disconnect()
     pass
 
-
-# api.include_router(auth.router)
-# api.include_router(user.router)
-
-# app.include_router(api)
 
 @app.get('/')
 async def index():
