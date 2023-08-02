c041229683ce8be641e2ac3d7ae2d73ee3a35299
007 <dr007cc@gmail.com>
2023-08-02 04:46:19 +0330

    fix: update stuff

--- config/webpack/config/path.ts
+++ config/webpack/config/path.ts
@@ -3,6 +3,6 @@ import { resolve } from 'path'
 const BASE_DIR = resolve(__dirname, '../../../')
 const APP_DIR = resolve(BASE_DIR, 'app')
 const DIST_DIR = resolve(BASE_DIR, 'static/dist/')
-const TEMPLATES_DIR = resolve(BASE_DIR, 'static/templates/include/')
+const TEMPLATES_DIR = resolve(BASE_DIR, 'static/templates/')
 
 export { BASE_DIR, APP_DIR, DIST_DIR, TEMPLATES_DIR, resolve }
