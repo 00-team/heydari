ef3f82a07d6fab2f4150529996b24413befc1c2b
007 <dr007cc@gmail.com>
2023-08-04 22:13:30 +0330

    feat: add some shared stuff

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 
 
 from fastapi import FastAPI, Request
+from fastapi.openapi.utils import get_openapi
 from fastapi.responses import HTMLResponse
 from fastapi.routing import APIRoute
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+import shared.logger
 from deps import get_ip
 from shared import settings
-from shared.errors import Error
+from shared.errors import Error, all_errors
 
 app = FastAPI(
     title='Heydari',
@@ -39,6 +41,19 @@ async def shutdown():
     pass
 
 
+@app.get('/rapidoc/', include_in_schema=False)
+async def rapidoc():
+    return HTMLResponse('''<!doctype html>
+    <html><head><meta charset="utf-8">
+    <script type="module" src="/static/rapidoc.js"></script></head><body>
+    <rapi-doc spec-url="/openapi.json" persist-auth="true"
+    bg-color="#040404" text-color="#f2f2f2" header-color="#040404"
+    primary-color="#ff8800" nav-text-color="#eee" font-size="largest"
+    allow-spec-url-load="false" allow-spec-file-load="false"
+    show-method-in-nav-bar="as-colored-block" response-area-height="500px"
+    show-header="false" /></body> </html>''')
+
+
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse(
@@ -52,3 +67,26 @@ async def index(request: Request):
 for route in app.routes:
     if isinstance(route, APIRoute):
         route.operation_id = route.name
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    schema = get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+    )
+
+    schema['errors'] = {}
+
+    for e in all_errors:
+        schema['errors'][e.code] = e.schema
+
+    app.openapi_schema = schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
