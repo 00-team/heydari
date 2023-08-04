eefbfc9ad0227b70851bd17a5c80c41e7c563e4a
007 <dr007cc@gmail.com>
2023-08-04 22:59:01 +0330

    fix: export record stuff

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -1,5 +1,7 @@
 from .common import BaseTable, metadata, model_dict
+from .record import RecordItemTable, RecordModel, RecordTable
 
 __all__ = [
     'BaseTable', 'metadata', 'model_dict',
+    'RecordTable', 'RecordModel', 'RecordItemTable',
 ]
