3fe19b5929e0ed4dc9344923d4f15d300c2cceda
007 <dr007cc@gmail.com>
2023-08-05 18:55:38 +0330

    fix: export BlogTagTable

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -1,11 +1,11 @@
-from .blog import BlogModel, BlogTable
+from .blog import BlogModel, BlogTable, BlogTagTable
 from .common import BaseTable, metadata, model_dict
 from .general import GeneralModel, GeneralTable
 from .record import RecordItemTable, RecordModel, RecordTable
 from .user import UserModel, UserPublic, UserTable
 
 __all__ = [
-    'BlogTable', 'BlogModel',
+    'BlogTable', 'BlogModel', 'BlogTagTable',
     'BaseTable', 'metadata', 'model_dict',
     'GeneralTable', 'GeneralModel',
     'RecordTable', 'RecordModel', 'RecordItemTable',
