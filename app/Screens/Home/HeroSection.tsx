bc07e1c2ce430bf1e6824f3cce999f092705a236
sadra <sadrataghavi1383@gmail.com>
2023-08-09 13:49:36 +0330

    fix(hero)

--- app/Screens/Home/HeroSection.tsx
+++ app/Screens/Home/HeroSection.tsx
@@ -33,11 +33,7 @@ const HeroSection: FC = () => {
                         صنایع تولیدی{' '}
                         <span className='section_title'>حیدری</span>
                     </div>
-                    <div
-                        className={`detail ${
-                            innerWidth >= 768 ? 'title_smaller' : 'title_small'
-                        }`}
-                    >
+                    <div className={`detail title_small`}>
                         ما مفتخریم در طول 40 سال فعالیت تولیدی، همواره کیفیت
                         برتر را جهت فراهم نمودن رضایت مشتریان بعنوان هدف اصلی در
                         نظر گرفته و در عرصه تولید ملی در زمینه صندلی آموزشی و
