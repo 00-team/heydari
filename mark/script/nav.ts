67c9a101690c7457d03714cae243a105147e9f6f
sadra <sadrataghavi1383@gmail.com>
2023-08-10 17:16:19 +0330

    feat(home)

--- mark/script/nav.ts
+++ mark/script/nav.ts
@@ -1,8 +1,43 @@
 export {}
-var x: number | null = null
 
-x = 12
+const navContainer = document.querySelector('nav.nav-container')
+const openSmallNav = document.querySelector('.small-open')
+const closeSmallNav = document.querySelector('.small-close')
 
-x *= 12
+let scrolled = false
+let isSmall = false
+let openSmall = false
 
-console.log(x)
+document.addEventListener('DOMContentLoaded', () => {
+    if (innerWidth < 1024) isSmall = true
+    render()
+
+    window.onresize = () => {
+        if (innerWidth < 1024) isSmall = true
+        else isSmall = false
+        render()
+    }
+
+    window.onscroll = () => {
+        if (scrollY > 10) scrolled = true
+        else scrolled = false
+        render()
+    }
+})
+
+openSmallNav.addEventListener('click', () => {
+    openSmall = true
+    render()
+})
+closeSmallNav.addEventListener('click', () => {
+    openSmall = false
+    render()
+})
+
+function render() {
+    let addedClass = `${scrolled ? 'scrolled' : ''} ${
+        isSmall ? 'small' : 'big'
+    } ${openSmall ? 'opensmall' : ''}`
+
+    navContainer.className = `nav-container ${addedClass}`
+}
