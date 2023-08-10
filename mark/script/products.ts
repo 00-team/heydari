718bdf3f68144cafea2261f198c375d947062f17
sadra <sadrataghavi1383@gmail.com>
2023-08-10 16:12:49 +0330

    feat(products): header slidein

--- mark/script/products.ts
+++ mark/script/products.ts
@@ -1,2 +1,26 @@
 export {}
-console.log('prodcuts')
+
+const productsElem = document.querySelector('.products-container#products')
+const productsHeader = document.querySelector('.products-header')
+
+const addClass = () => {
+    productsHeader.className += ' active'
+
+    return
+}
+
+window.onload = () => {
+    var observer = new IntersectionObserver(
+        ([entry]) => {
+            if (entry && entry.isIntersecting) {
+                addClass()
+                observer.disconnect()
+            }
+        },
+        {
+            threshold: 0.4,
+        }
+    )
+
+    observer.observe(productsElem)
+}
