1b371de332300b71be779f79a53a87b67f42a429
sadra <sadrataghavi1383@gmail.com>
2023-10-18 15:04:47 +0330

    feat(products)

--- app/Screens/Products/index.tsx
+++ app/Screens/Products/index.tsx
@@ -19,6 +19,22 @@ const productImg10 = '/static/image/products/10.webp'
 const productImg11 = '/static/image/products/11.webp'
 const productImg12 = '/static/image/products/12.webp'
 const productImg13 = '/static/image/products/13.webp'
+const productImg14 = '/static/image/products/14.webp'
+const productImg15 = '/static/image/products/15.webp'
+const productImg16 = '/static/image/products/16.webp'
+const productImg17 = '/static/image/products/17.webp'
+const productImg18 = '/static/image/products/18.webp'
+const productImg19 = '/static/image/products/19.webp'
+const productImg20 = '/static/image/products/20.webp'
+const productImg21 = '/static/image/products/21.webp'
+const productImg22 = '/static/image/products/22.webp'
+const productImg23 = '/static/image/products/23.webp'
+const productImg24 = '/static/image/products/24.webp'
+const productImg25 = '/static/image/products/25.webp'
+const productImg26 = '/static/image/products/26.webp'
+const productImg27 = '/static/image/products/27.webp'
+const productImg28 = '/static/image/products/28.webp'
+const productImg29 = '/static/image/products/29.webp'
 
 const imgs = [
     productImg1,
@@ -34,6 +50,22 @@ const imgs = [
     productImg11,
     productImg12,
     productImg13,
+    productImg14,
+    productImg15,
+    productImg16,
+    productImg17,
+    productImg18,
+    productImg19,
+    productImg20,
+    productImg21,
+    productImg22,
+    productImg23,
+    productImg24,
+    productImg25,
+    productImg26,
+    productImg27,
+    productImg28,
+    productImg29,
 ]
 
 const Products: FC = () => {
