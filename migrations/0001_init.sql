3cc709975f3cb5d89a1df2ebf2085070bf212de5
007 <dr007cc@gmail.com>
2024-06-09 21:48:41 +0330

    fix(api): set product code to be unique

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -20,7 +20,7 @@ create table if not exists product_tags (
 create table if not exists products (
     id integer primary key not null,
     name text not null,
-    code text not null,
+    code text unique not null,
     detail text not null default "",
     timestamp integer not null,
     thumbnail text,
