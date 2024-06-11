aaf2026bc1a6db7a85f48a39af9000e2fe640f95
007 <dr007cc@gmail.com>
2024-06-11 03:51:35 +0330

    feat(admin): clear confact after 10s

--- admin/comps/confact.tsx
+++ admin/comps/confact.tsx
@@ -1,7 +1,7 @@
 // conf-act or confirm action :)
 
 import './style/confact.scss'
-import { Component, JSX, createSignal } from 'solid-js'
+import { Component, JSX, createEffect, createSignal } from 'solid-js'
 
 type Props = {
     icon: () => JSX.Element
@@ -13,6 +13,15 @@ type Props = {
 const Confact: Component<Props> = P => {
     const [stage2, setStage2] = createSignal(false)
     let timeout = 0
+    let timer = 0
+
+    createEffect(() => {
+        clearTimeout(timer)
+        if (!stage2()) return
+        timer = setTimeout(() => {
+            setStage2(false)
+        }, 10e3)
+    })
 
     return (
         <button
@@ -20,6 +29,7 @@ const Confact: Component<Props> = P => {
             style={{ '--tsd': (P.timer_ms || 1000) + 'ms', '--color': P.color }}
             class='cmp-confact icon'
             classList={{ stage2: stage2() }}
+            title={stage2() ? 'right click to unactivate' : 'hold to activate'}
             oncontextmenu={e => {
                 e.preventDefault()
                 setStage2(false)
