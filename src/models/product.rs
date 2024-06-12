48ce235e38dae820da6b9d327cdc968458b67976
007 <dr007cc@gmail.com>
2024-06-12 09:31:14 +0330

    feat(api): impl FromRow manually

--- src/models/product.rs
+++ src/models/product.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use sqlx::{sqlite::SqliteRow, Row};
 use utoipa::ToSchema;
 
 use super::{from_request, sql_enum, JsonStr};
@@ -26,7 +27,7 @@ pub struct ProductTag {
 
 from_request!(ProductTag, "product_tags");
 
-#[derive(Debug, Serialize, Deserialize, sqlx::FromRow, ToSchema, Default)]
+#[derive(Debug, Serialize, Deserialize, ToSchema, Default)]
 pub struct Product {
     pub id: i64,
     pub kind: ProductKind,
@@ -42,3 +43,31 @@ pub struct Product {
 }
 
 from_request!(Product, "products");
+
+impl<'r> sqlx::FromRow<'r, SqliteRow> for Product {
+    fn from_row(row: &'r SqliteRow) -> Result<Self, sqlx::Error> {
+        let id = row.get::<i64, _>("id");
+        let kind = row.get::<i64, _>("kind");
+        let name = row.get::<String, _>("name");
+        let code = row.get::<String, _>("code");
+        let detail = row.get::<String, _>("detail");
+        let timestamp = row.get::<i64, _>("timestamp");
+        let thumbnail = row.get::<Option<String>, _>("thumbnail");
+        let photos = row.get::<String, _>("photos");
+        let tag_leg = row.get::<Option<i64>, _>("tag_leg");
+        let tag_bed = row.get::<Option<i64>, _>("tag_bed");
+
+        Ok(Product {
+            id,
+            kind: kind.into(),
+            name,
+            code,
+            detail,
+            timestamp,
+            thumbnail,
+            photos: photos.into(),
+            tag_leg,
+            tag_bed,
+        })
+    }
+}
