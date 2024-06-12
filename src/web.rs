2c629bf8e5084223d1c05fd915206da39fbea8f7
007 <dr007cc@gmail.com>
2024-06-12 10:00:37 +0330

    fix(api): web

--- src/web.rs
+++ src/web.rs
@@ -3,9 +3,10 @@ use actix_web::http::header::ContentType;
 use actix_web::middleware::NormalizePath;
 use actix_web::web::{Data, Path, Query};
 use actix_web::{get, FromRequest, HttpRequest, HttpResponse, Scope};
+use itertools::Itertools;
 use minijinja::{context, path_loader, Environment};
 use serde::Deserialize;
-use sqlx::{Sqlite, SqlitePool};
+use sqlx::SqlitePool;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
@@ -19,6 +20,25 @@ type Response = Result<HttpResponse, AppErr>;
 async fn home(env: Data<Environment<'static>>) -> Response {
     let result = env.get_template("home/index.html")?.render(())?;
     Ok(HttpResponse::Ok().content_type(ContentType::html()).body(result))
+}
+
+type Tags = HashMap<i64, (String, i64)>;
+async fn get_tags(
+    ids: Vec<Option<i64>>, pool: &SqlitePool,
+) -> Result<Tags, AppErr> {
+    let ids = ids.iter().flatten().unique().join(",");
+
+    if ids.len() == 0 {
+        return Ok(Tags::new());
+    }
+
+    let tags = sqlx::query_as! {
+        ProductTag, "select * from product_tags where id in (?)", ids
+    }
+    .fetch_all(pool)
+    .await?;
+
+    Ok(tags.iter().map(|v| (v.id, (v.name.clone(), v.count))).collect::<Tags>())
 }
 
 #[derive(Deserialize, Debug)]
@@ -44,50 +64,84 @@ async fn products(
     let q = Query::<ProductsQuery>::extract(&rq).await;
     let mut sort = "desc";
     let mut offset = 0;
-    match q {
-        Ok(v) => {
-            if matches!(v.sort, Some(Sort::Asc)) {
-                sort = "asc";
+    let mut filter = Vec::<String>::new();
+
+    if let Ok(q) = &q {
+        if matches!(q.sort, Some(Sort::Asc)) {
+            sort = "asc";
+        }
+
+        if let Some(p) = q.page {
+            offset = p * 32;
+        }
+
+        if let Some(kind) = &q.kind {
+            filter.push(format!("kind = {}", kind.clone() as i64));
+
+            if let Some(leg) = q.leg {
+                filter.push(format!("tag_leg = {leg}"));
             }
-            if let Some(p) = v.page {
-                offset = p * 32;
+
+            if let Some(bed) = q.bed {
+                filter.push(format!("tag_bed = {bed}"));
             }
         }
-        Err(_) => {}
     }
 
-    // let x = "select * from products order by timestamp desc limit 32 offset ?";
-    let products = sqlx::query_as! {
-        Product,
-        "select * from products order by timestamp desc limit 32 offset ?",
-        offset
-    }
+    let cond = if filter.len() > 0 {
+        "where ".to_string() + &filter.join(" AND ")
+    } else {
+        String::new()
+    };
+
+    let products: Vec<Product> = sqlx::query_as(&format!(
+        "select * from products {} order by timestamp {} limit 32 offset ?",
+        cond, sort
+    ))
+    .bind(offset)
     .fetch_all(&state.sql)
     .await?;
-    let tags = sqlx::query_as! {
-        ProductTag,
-        "select * from product_tags"
-    }
-    .fetch_all(&state.sql)
-    .await?;
 
-    let tags = tags.iter().map(|v| (v.id, v.name.clone())).collect::<HashMap<i64, String>>();
+    let tags = get_tags(
+        products.iter().map(|p| [p.tag_leg, p.tag_bed]).flatten().collect(),
+        &state.sql,
+    )
+    .await;
 
-    // sqlx::query_as::<Sqlite, Product>(x);
-    // let products: Vec<Product> = sqlx::query_as(x).fetch_all(&state.sql).await?;
     let result = env.get_template("products/index.html")?.render(context! {
         products => products,
         tags => tags,
     })?;
-    // log::info!("result: {result}");
+
     Ok(HttpResponse::Ok().content_type(ContentType::html()).body(result))
 }
 
 #[get("/products/{id}")]
 async fn product(
-    path: Path<(i64,)>, env: Data<Environment<'static>>,
+    rq: HttpRequest, env: Data<Environment<'static>>, state: Data<AppState>,
 ) -> Response {
-    let result = env.get_template("product/index.html")?.render(())?;
+    let path = Path::<(i64,)>::extract(&rq).await;
+    if path.is_err() {
+        return Ok(HttpResponse::NotFound()
+            .content_type(ContentType::html())
+            .body("404"));
+    }
+
+    let id = path.unwrap().0;
+    let product = sqlx::query_as! {
+        Product, "select * from products where id = ?", id
+    }
+    .fetch_one(&state.sql)
+    .await?;
+
+    let tags = vec![product.tag_leg, product.tag_bed];
+    let tags = get_tags(tags, &state.sql).await;
+
+    let result = env.get_template("product/index.html")?.render(context! {
+        product => product,
+        tags => tags,
+    })?;
+
     Ok(HttpResponse::Ok().content_type(ContentType::html()).body(result))
 }
 
