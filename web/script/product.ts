b122a258bbdecf81f637303a81076ae90de5becb
sadra <sadrataghavi1383@gmail.com>
2024-07-03 13:21:57 +0330

    feat(product): zoom

--- web/script/product.ts
+++ web/script/product.ts
@@ -1,6 +1,5 @@
 export {}
 
-
 const main = document.querySelector<HTMLImageElement>('.main-img')
 const imgs = document.querySelectorAll('.other-img')
 
@@ -9,3 +8,81 @@ imgs.forEach((img: HTMLImageElement) => {
         main.src = img.src
     })
 })
+
+function magnify(img: HTMLImageElement, zoom) {
+    const parent = img.parentElement
+    const glass = document.createElement('DIV')
+    glass.setAttribute('class', 'img-magnifier-glass')
+
+    parent.insertBefore(glass, img)
+
+    const parentWidth = parent.offsetWidth
+    const parentHeight = parent.offsetHeight
+
+    glass.style.backgroundImage = `url('${img.src}')`
+    glass.style.backgroundRepeat = 'no-repeat'
+    glass.style.backgroundSize = `cover`
+
+    let bw = 3
+    let w = glass.offsetWidth / 2
+    let h = glass.offsetHeight / 2
+
+    glass.addEventListener('mousemove', moveMagnifier)
+    img.addEventListener('mousemove', moveMagnifier)
+    glass.addEventListener('touchmove', moveMagnifier)
+    img.addEventListener('touchmove', moveMagnifier)
+
+    function moveMagnifier(e: MouseEvent | TouchEvent) {
+        let pos, x, y
+        e.preventDefault()
+        pos = getCursorPos(e)
+        x = pos.x
+        y = pos.y
+
+        if (x > parentWidth - (w / zoom) * 0.1) {
+            x = parentWidth - w / zoom
+        }
+        if (x < w / zoom) {
+            x = w / zoom
+        }
+        if (y > parentHeight - (h / zoom) * 0.1) {
+            y = parentHeight - h / zoom
+        }
+        if (y < (h / zoom) * 0.1) {
+            y = h / zoom
+        }
+        glass.style.left = `${x - w}px`
+        glass.style.top = `${y - h}px`
+
+        const bgPosX = (x / parentWidth) * parentWidth * zoom - w + bw
+        const bgPosY = (y / parentHeight) * parentHeight * zoom - h + bw
+
+        glass.style.backgroundPosition = `-${bgPosX}px -${bgPosY}px`
+        glass.style.backgroundSize = `${parentWidth * zoom}px ${
+            parentHeight * zoom
+        }px`
+        glass.style.display = 'block'
+    }
+
+    function getCursorPos(e: MouseEvent | TouchEvent) {
+        let a,
+            x = 0,
+            y = 0
+        a = parent.getBoundingClientRect()
+
+        if (e instanceof TouchEvent) {
+            x = e.touches[0].clientX - a.left
+            y = e.touches[0].clientY - a.top
+        } else {
+            x = e.clientX - a.left
+            y = e.clientY - a.top
+        }
+
+        x = (x / (a.right - a.left)) * parentWidth
+        y = (y / (a.bottom - a.top)) * parentHeight
+
+        return { x: x, y: y }
+    }
+}
+
+magnify(main, 3)
