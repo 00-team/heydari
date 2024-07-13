05369cbfc64b94078275f2036df57b3705c2d941
007 <dr007cc@gmail.com>
2024-07-13 13:01:32 +0330

    feat(api): add blogs sitemap

--- src/sitemap.rs
+++ src/sitemap.rs
@@ -1,4 +1,4 @@
-use crate::AppState;
+use crate::{utils, AppState};
 use actix_web::dev::{ConnectionInfo, HttpServiceFactory};
 use actix_web::http::header::ContentType;
 use actix_web::middleware::NormalizePath;
@@ -32,6 +32,16 @@ async fn web(conn: ConnectionInfo) -> HttpResponse {
     HttpResponse::Ok()
         .content_type(ContentType::xml())
         .body(r#"<?xml version="1.0" encoding="UTF-8"?>"#.to_string() + &result)
+}
+
+#[get("/sitemap-blogs.xml")]
+async fn blogs(conn: ConnectionInfo) -> HttpResponse {
+    let base_url = format!("{}://{}/blogs", conn.scheme(), conn.host());
+
+    let url = format!("/blogs-sitemap/?base_url={base_url}");
+    let sitemap = utils::simurgh_request(&url).await.unwrap_or_default();
+
+    HttpResponse::Ok().content_type(ContentType::xml()).body(sitemap)
 }
 
 #[get("/sitemap-products.xml")]
@@ -80,6 +90,7 @@ async fn index(conn: ConnectionInfo) -> HttpResponse {
             xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
             sitemap { loc { "{host}/sitemap-web.xml/" } }
             sitemap { loc { "{host}/sitemap-products.xml/" } }
+            sitemap { loc { "{host}/sitemap-blogs.xml/" } }
         }
     }
     .render();
@@ -95,4 +106,5 @@ pub fn router() -> impl HttpServiceFactory {
         .service(index)
         .service(web)
         .service(products)
+        .service(blogs)
 }
