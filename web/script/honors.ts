9d70dce55a8e656d5c88563f0a390d42a23aa723
sadra <sadrataghavi1383@gmail.com>
2024-07-20 14:05:39 +0330

    feat(honors)

--- web/script/honors.ts
+++ web/script/honors.ts
@@ -12,11 +12,36 @@ const addClass = () => {
     return
 }
 
+const honors = document.querySelectorAll('.honor-container')
+
+let current = 1
+
+const setClass = index => {
+    honors.forEach((content, idx0) => {
+        if (index === idx0)
+            return (content.className = 'honor-container active')
+        if (index - 1 === idx0)
+            return (content.className = 'honor-container prev')
+        if (index + 1 === idx0)
+            return (content.className = 'honor-container next')
+        if (index === honors.length - 1 && idx0 === 0)
+            return (content.className = 'honor-container next')
+        return (content.className = 'honor-container')
+    })
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     var observer = new IntersectionObserver(
         ([entry]) => {
             if (entry && entry.isIntersecting) {
                 addClass()
+
+                setClass(current) // Initialize with the first content as active
+                setInterval(() => {
+                    current = (current + 1) % honors.length // Circular increment
+                    setClass(current)
+                }, 3000)
+
                 observer.disconnect()
             }
         },
