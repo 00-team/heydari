7830f60077e55e6f1059b77129e2286035278455
sadra <sadrataghavi1383@gmail.com>
2024-09-02 19:50:37 +0330

    feat(product)

--- web/script/product.ts
+++ web/script/product.ts
@@ -8,3 +8,43 @@ imgs.forEach((img: HTMLImageElement) => {
         main.src = img.src
     })
 })
+
+const aboutProduct = document.querySelector<HTMLElement>('.product-about')
+
+const aboutSelect = aboutProduct.querySelector<HTMLElement>('.about-select')
+const aboutWrapper = aboutProduct.querySelector<HTMLElement>('.about-wrapper')
+
+const InfoButton = aboutSelect.querySelector<HTMLButtonElement>('button.info')
+const AboutButton = aboutSelect.querySelector<HTMLButtonElement>('button.about')
+
+const aboutSection = aboutWrapper.querySelector<HTMLElement>('.about')
+const infoSection = aboutWrapper.querySelector<HTMLElement>('.info')
+
+let activeSection: 'info' | 'about' = 'about'
+
+function update() {
+    if (activeSection === 'about') {
+        AboutButton.className = 'title_small about active'
+        InfoButton.className = 'title_small info'
+
+        aboutSection.className = 'about title_smaller active'
+        infoSection.className = 'info title_smaller'
+    } else {
+        AboutButton.className = 'title_small about '
+        InfoButton.className = 'title_small info active'
+
+        aboutSection.className = 'about title_smaller '
+        infoSection.className = 'info title_smaller active'
+    }
+}
+
+update()
+
+InfoButton.addEventListener('click', () => {
+    activeSection = 'info'
+    update()
+})
+AboutButton.addEventListener('click', () => {
+    activeSection = 'about'
+    update()
+})
