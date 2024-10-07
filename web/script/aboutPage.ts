1bd2e4bfb7fbeca689d61c823b913f63015e4026
sadra <sadrataghavi1383@gmail.com>
2024-10-07 13:31:00 +0330

    feat(about)

--- web/script/aboutPage.ts
+++ web/script/aboutPage.ts
@@ -2,9 +2,14 @@ const honorsSection = document.querySelector<HTMLElement>(
     '.about-page-honors#honors'
 )
 const honorTitle = honorsSection.querySelector<HTMLElement>('.honors-title')
-
 const honorsWrapper =
     honorsSection.querySelector<HTMLElement>('.honors-wrapper')
+
+const projectsSection = document.querySelector<HTMLElement>(
+    '.about-page-projects'
+)
+const projectTitle =
+    projectsSection.querySelector<HTMLElement>('.project-title')
 
 document.addEventListener(
     'DOMContentLoaded',
@@ -24,6 +29,21 @@ document.addEventListener(
         )
 
         observer.observe(honorsSection)
+
+        var observer2 = new IntersectionObserver(
+            ([entry]) => {
+                if (entry && entry.isIntersecting) {
+                    projectTitle.className += ' active'
+
+                    observer2.disconnect()
+                }
+            },
+            {
+                rootMargin: '-150px',
+            }
+        )
+
+        observer2.observe(projectsSection)
     },
     false
 )
