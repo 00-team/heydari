d9bd0dd472c8fea603a13e723341aab538003380
sadra <sadrataghavi1383@gmail.com>
2024-10-29 12:35:57 +0330

    feat(home)

--- web/script/home.ts
+++ web/script/home.ts
@@ -1,5 +1,11 @@
 export {}
 
+const welcomerContainer = document.querySelector<HTMLElement>('.welcomer')
+
+welcomerContainer.addEventListener('click', () => {
+    welcomerContainer.style.display = 'none'
+})
+
 const stickyButton = document.querySelector<HTMLElement>('a.main.hero-button')
 
 stickyButton.addEventListener('mousemove', e => {
