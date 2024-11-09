4428003d4e9d3c612a170648829eadf7c5d46321
sadra <sadrataghavi1383@gmail.com>
2024-11-09 17:44:43 +0330

    feat(icoons)

--- admin/icons.tsx
+++ admin/icons.tsx
@@ -14,6 +14,35 @@ export const CircleAlertIcon = () => (
     </svg>
 )
 
+export const ChairIcon = () => (
+    <svg
+        stroke='currentColor'
+        fill='currentColor'
+        stroke-width='0'
+        viewBox='0 0 448 512'
+        height='1em'
+        width='1em'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path d='M248 48V256h48V58.7c23.9 13.8 40 39.7 40 69.3V256h48V128C384 57.3 326.7 0 256 0H192C121.3 0 64 57.3 64 128V256h48V128c0-29.6 16.1-55.5 40-69.3V256h48V48h48zM48 288c-12.1 0-23.2 6.8-28.6 17.7l-16 32c-5 9.9-4.4 21.7 1.4 31.1S20.9 384 32 384l0 96c0 17.7 14.3 32 32 32s32-14.3 32-32V384H352v96c0 17.7 14.3 32 32 32s32-14.3 32-32V384c11.1 0 21.4-5.7 27.2-15.2s6.4-21.2 1.4-31.1l-16-32C423.2 294.8 412.1 288 400 288H48z'></path>
+    </svg>
+)
+
+export const StorageIcon = () => (
+    <svg
+        stroke='currentColor'
+        fill='currentColor'
+        stroke-width='0'
+        viewBox='0 0 24 24'
+        height='1em'
+        width='1em'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path fill='none' d='M0 0h24v24H0V0z'></path>
+        <path d='M2 20h20v-4H2v4zm2-3h2v2H4v-2zM2 4v4h20V4H2zm4 3H4V5h2v2zm-4 7h20v-4H2v4zm2-3h2v2H4v-2z'></path>
+    </svg>
+)
+
 export const CircleCheckIcon = () => (
     <svg
         xmlns='http://www.w3.org/2000/svg'
