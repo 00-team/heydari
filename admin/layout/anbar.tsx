c6097dd07c20d8c4c75762f18e162fd5e6f5de0e
sadra <sadrataghavi1383@gmail.com>
2024-11-09 12:42:46 +0330

    fix(app)

--- admin/layout/anbar.tsx
+++
@@ -1,9 +0,0 @@
-import { Component } from 'solid-js'
-
-import './style/anbar.scss'
-
-const Anbar: Component<{}> = props => {
-    return <div class='anbar-container'></div>
-}
-
-export default Anbar
