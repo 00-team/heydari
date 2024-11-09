96c7029d82b7c3ad727e31204256dd31eb9f94c7
sadra <sadrataghavi1383@gmail.com>
2024-11-09 11:56:58 +0330

    feat(anbar): init tsx

---
+++ admin/layout/anbar.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'solid-js'
+
+import './style/anbar.scss'
+
+const Anbar: Component<{}> = props => {
+    return <div class='anbar-container'></div>
+}
+
+export default Anbar
