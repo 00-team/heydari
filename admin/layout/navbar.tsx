4f214e6b037ec5d1889224ff976ac95d014f83e2
sadra <sadrataghavi1383@gmail.com>
2025-01-04 16:02:29 +0330

    feat(nav)

--- admin/layout/navbar.tsx
+++ admin/layout/navbar.tsx
@@ -1,4 +1,4 @@
-import { A } from '@solidjs/router'
+import { A, reload } from '@solidjs/router'
 import {
     ChairIcon,
     ExitIcon,
@@ -7,16 +7,95 @@ import {
     TagIcon,
     TrashIcon,
 } from 'icons'
+import { httpx, Perm } from 'shared'
 
-import { Show } from 'solid-js'
-import { Perms, self } from 'store'
+import { For, JSX, onMount, Show } from 'solid-js'
+import { createStore } from 'solid-js/store'
+import { Perms, self, setSelf } from 'store'
 import { setPopup } from 'store/popup'
 import './style/navbar.scss'
 
-export default () => {
-    console.log(self)
+type linkType = {
+    link: string
+    title: string
+    icon: JSX.Element
+    perm: Perm
+}
 
-    function logout() {}
+const links: linkType[] = [
+    {
+        icon: <ChairIcon />,
+        link: '/',
+        title: 'محصولات',
+        perm: Perms.V_PRODUCT,
+    },
+    {
+        icon: <TagIcon />,
+        link: '/product-tags/',
+        title: 'تگ محصولات',
+        perm: Perms.V_PRODUCT_TAG,
+    },
+    {
+        icon: <StorageIcon />,
+        link: '/storage/',
+        title: 'انبار',
+        perm: Perms.V_MATERIAL,
+    },
+    {
+        icon: <PersonIcon />,
+        link: '/users/',
+        title: 'ادمین ها',
+        perm: Perms.V_USER,
+    },
+]
+
+export default () => {
+    const [state, setState] = createStore({
+        active: 0,
+    })
+
+    onMount(() => {
+        let pathname = location.pathname.split('/')
+
+        let adminIndex = pathname.findIndex(str => str === 'admin')
+
+        if (adminIndex <= -1) return
+
+        let activeIndex = pathname[adminIndex + 1]
+
+        let activeLink = links.findIndex(
+            link => link.link.split('/').pop() === activeIndex
+        )
+
+        activeLink >= 0 && setState({ active: activeLink })
+    })
+
+    function logout() {
+        httpx({
+            method: 'POST',
+            url: '/api/user/logout/',
+            onLoad(x) {
+                if (x.status !== 200) return
+
+                setSelf({
+                    loged_in: false,
+                    fetch: true,
+                    perms: new Perms([]),
+                    user: {
+                        id: 0,
+                        phone: '',
+                        name: '',
+                        token: null,
+                        photo: null,
+                        admin: [],
+                        banned: false,
+                    },
+                })
+
+                reload()
+            },
+        })
+    }
     return (
         <nav class='navbar-fnd'>
             <div class='admin-info title_small'>
@@ -35,33 +114,26 @@ export default () => {
                 >
                     <ExitIcon />
                 </button>
-                {self.user.name || 'ادمین حیدری'}
+                {self.user.name || self.user.phone}
             </div>
             <div class='links'>
-                <Show when={self.perms.check(Perms.V_PRODUCT)}>
-                    <A href='/products/' class='title_smaller'>
-                        <ChairIcon />
-                        محصولات
-                    </A>
-                </Show>
-                <Show when={self.perms.check(Perms.V_PRODUCT_TAG)}>
-                    <A href='/product-tags/' class='title_smaller'>
-                        <TagIcon />
-                        تگ مصحولات
-                    </A>
-                </Show>
-                <Show when={self.perms.check(Perms.V_MATERIAL)}>
-                    <A href='/storage/' class='title_smaller'>
-                        <StorageIcon />
-                        انبار
-                    </A>
-                </Show>
-                <Show when={self.perms.check(Perms.V_USER)}>
-                    <A href='/users/' class='title_smaller'>
-                        <PersonIcon />
-                        ادمین ها
-                    </A>
-                </Show>
+                <For each={links}>
+                    {(link, index) => (
+                        <Show when={self.perms.check(link.perm)}>
+                            <A
+                                href={link.link}
+                                class='title_smaller'
+                                classList={{ active: state.active == index() }}
+                                onclick={() => setState({ active: index() })}
+                            >
+                                <span>
+                                    {link.icon}
+                                    {link.title}
+                                </span>
+                            </A>
+                        </Show>
+                    )}
+                </For>
             </div>
             <img
                 class='nav-logo-container'
