b4da177a8eeb4ae755c4735772bb515c438e2a53
sadra <sadrataghavi1383@gmail.com>
2025-11-10 15:00:53 +0330

    feat(orders): ts

--- web/script/account_orders.ts
+++ web/script/account_orders.ts
@@ -4,3 +4,8 @@ for (let e of document.querySelectorAll('div.aUTpGc129q')) {
     let d = new Date(parseInt(e.textContent.trim()) * 1e3)
     e.textContent = d.toLocaleDateString('fa-IR')
 }
+for (let e of document.querySelectorAll('#fix-toman-amount')) {
+    if (!e.textContent) continue
+
+    e.textContent = parseInt(e.textContent).toLocaleString()
+}
